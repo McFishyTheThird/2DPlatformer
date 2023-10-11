a3d027a831231ff9c61bc3cec67de34035e6f41d
William <william.lignell@elev.ga.ntig.se>
2023-10-11 16:13:50 +0200

    Commit

---
+++ Assets/GeoffreyController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoffreyController : MonoBehaviour
+{
+    [SerializeField]
+    float jumpForce = 300;
+    [SerializeField]
+    Transform feet;
+    float groundRadius = 0.2f;
+    [SerializeField]
+    int speed = 10;
+    [SerializeField]
+    LayerMask groundLayer;
+
+    Vector2 bottomColliderSize = Vector2.zero;
+    Vector2 footPosition;
+    bool hasReleasedJumpButton = true;
+
+    Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+
+        Vector2 movement = new Vector2(moveX, 0);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        transform.Translate(movement);
+        
+        bool isGround = Physics2D.OverlapBox(GetFootPosition(), GetFootSize(), 0, groundLayer);
+        if (Input.GetAxisRaw("Jump") > 0 && hasReleasedJumpButton == true && isGround)
+        {
+            rigidBody.AddForce(Vector2.up * jumpForce);
+            hasReleasedJumpButton = false;
+            Debug.Log(hasReleasedJumpButton);
+        }
+        if (Input.GetAxisRaw("Jump") == 0)
+        {
+            hasReleasedJumpButton = true;
+            Debug.Log(hasReleasedJumpButton);
+
+        }
+    }
+
+    private Vector2 GetFootPosition()
+    {
+        float height = GetComponent<Collider2D>().bounds.size.y;
+        return transform.position + Vector3.down * height / 2;
+    }
+    private Vector2 GetFootSize()
+    {
+        return new Vector2(GetComponent<Collider2D>().bounds.size.x * 0.9f, 0.1f);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(GetFootPosition(), GetFootSize());
+    }
+
+}
